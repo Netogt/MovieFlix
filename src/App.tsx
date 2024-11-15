4661b1b15cabc0c066c00a05cef99fbe421f478f
Luiz Neto <luizneton13@gmail.com>
2024-11-15 16:12:41 -0300

    adicionando a responsividade do slide

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ export default function App() {
         <>
             <SlideMovies />
             <SectionMovies />
+            <SectionMovies />
+            <SectionMovies />
         </>
     )
 }
--- src/components/PosterSlideMovie.tsx
+++ src/components/PosterSlideMovie.tsx
@@ -1,5 +1,5 @@
 
-export default function PosterSlideMovie({ backdrop_path, title, original_name, overview, release_date, vote_average }: any){
+export default function PosterSlideMovie({ backdrop_path, title, original_name, overview, release_date, vote_average }: any) {
 
     return (
         <div className="postSlideMovie" >
@@ -7,10 +7,16 @@ export default function PosterSlideMovie({ backdrop_path, title, original_name, 
                 <div className="slideBoxTitle">
                     <h2>{title ? title : original_name}</h2>
                     <p>{overview}</p>
+                    <p className="continue">continua...</p>
                 </div>
                 <div className="slideBoxDate">
-                    <p>*{vote_average}</p>
-                    <p>{release_date}</p>
+                    <p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                        {vote_average}
+                    </p>
+                    {release_date && <p>{release_date}</p>}
                 </div>
             </div>
             <img src={`https://image.tmdb.org/t/p/original/${backdrop_path}`} />
